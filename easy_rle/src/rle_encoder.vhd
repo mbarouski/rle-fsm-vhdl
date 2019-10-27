db64de61b28b42fe4a4677d53afb23a02ae19e0c
maximka777 <maksim.borovskij@yandex.ru>
2019-10-28 00:31:33 +0300

    1 tick encoder

--- easy_rle/src/rle_encoder.vhd
+++ easy_rle/src/rle_encoder.vhd
@@ -39,47 +39,32 @@ architecture rle_encoder of rle_encoder is
 	signal current_number: SIGNED(N-1 downto 0) := (others => '0');
 	signal previous_number: SIGNED(N-1 downto 0) := (others => '0');
 	
-	signal current_counter: UNSIGNED(N-1 downto 0) := (0 => '1', others => '0');
-	signal previous_counter: UNSIGNED(N-1 downto 0) := (0 => '1', others => '0');
+	signal inner_counter: UNSIGNED(N-1 downto 0) := (0 => '1', others => '0');
 	
 	signal sequence_finished: STD_LOGIC := '0';
+begin
+	current_number <= signed(input);
 	
-	signal stage: STD_LOGIC := '0';
-begin	
-	in_out_process: process(clk, enable)
+	p: process(clk, enable)
 	begin
 		if enable = '1' then 
-			if rising_edge(clk) then
-				if stage = '0' then	   
-					current_number <= signed(input);
-				else
-					previous_number <= current_number;
-				end if;							 	  
-			elsif falling_edge(clk) then 
-				if stage = '0' then	   				 		  
-					if current_number = previous_number then	 
-						current_counter <= current_counter + 1;
-						sequence_finished <= '0';
-						stage <= not stage;
-					else
-						previous_counter <= current_counter;
-						stage <= not stage;
-						if current_counter > 0 then	
-							sequence_finished <= '1';
-						end if;					 
-					end if;
-				else
-					output <= STD_LOGIC_VECTOR(previous_number);
-					counter <= STD_LOGIC_VECTOR(previous_counter);
-					
-					if sequence_finished = '1' then
-						current_counter <= (0 => '1', others => '0');
-					end if;															   
-					
-					read <= sequence_finished;
-					stage <= not stage;
-				end if;			   
+			if rising_edge(clk) then	  				 		  
+				if current_number = previous_number then	 
+					inner_counter <= inner_counter + 1;
+					sequence_finished <= '0';
+				else				
+					sequence_finished <= '1'; 		 			 
+				end if;						 	  
+			elsif falling_edge(clk) then  
+				if sequence_finished = '1' then
+					inner_counter <= (0 => '1', others => '0'); 
+					previous_number <= current_number;	
+				end if;		  		   
 			end if;
 		end if;
-	end process in_out_process;
+	end process p;	  
+	
+	output <= STD_LOGIC_VECTOR(previous_number);
+	counter <= STD_LOGIC_VECTOR(inner_counter);	
+	read <= sequence_finished;
 end rle_encoder;
