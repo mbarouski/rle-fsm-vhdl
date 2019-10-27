cb18779dd74ba94012ea8725619e8ea78ed84fa3
maximka777 <maksim.borovskij@yandex.ru>
2019-10-27 22:44:26 +0300

    Merge commit

---
+++ easy_rle/src/TestBench/frequency_divider_TB_runtest.do
@@ -0,0 +1,11 @@
+SetActiveLib -work
+comp -include "$dsn\src\frequency_divider.vhd" 
+comp -include "$dsn\src\TestBench\frequency_divider_TB.vhd" 
+asim +access +r TESTBENCH_FOR_frequency_divider 
+wave 
+wave -noreg clk_in
+wave -noreg clk_out
+# The following lines can be used for timing simulation
+# acom <backannotated_vhdl_file_name>
+# comp -include "$dsn\src\TestBench\frequency_divider_TB_tim_cfg.vhd" 
+# asim +access +r TIMING_FOR_frequency_divider 
---
+++ easy_rle/src/frequency_divider.vhd
@@ -0,0 +1,52 @@
+-------------------------------------------------------------------------------
+--
+-- Title       : frequency_divider
+-- Design      : easy_rle
+-- Author      : maxim
+-- Company     : none
+--
+-------------------------------------------------------------------------------
+--
+-- File        : frequency_divider.vhd
+-- Generated   : Sun Oct 27 19:50:01 2019
+-- From        : interface description file
+-- By          : Itf2Vhdl ver. 1.22
+--
+-------------------------------------------------------------------------------
+--
+-- Description :   Frequency divider
+--
+-------------------------------------------------------------------------------	
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all; 
+use ieee.numeric_std.all;
+
+entity frequency_divider is
+	generic(
+		N : integer := 2);
+	port(
+		clk_in : in STD_LOGIC;
+		clk_out : out STD_LOGIC
+		);
+end frequency_divider;						  
+
+architecture frequency_divider of frequency_divider is	
+	signal counter : integer := 0;
+begin 
+	p: process(clk_in)
+	begin
+		if rising_edge(clk_in) then 
+			if counter = 0 then
+				clk_out <= '1';
+			end if;				   
+		elsif falling_edge(clk_in) then 	
+			if counter = N-1 then
+				clk_out <= '0';
+				counter <= 0;
+			else 
+				counter <= counter + 1;
+			end if;					
+		end if;
+	end process p;
+end frequency_divider;
