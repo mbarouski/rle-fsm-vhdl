b24f8355ede40958920f2cfe0174d058f6a2efcd
maximka777 <maksim.borovskij@yandex.ru>
2019-11-30 14:06:42 +0300

    ram_unit_x is in the correct place

---
+++ fsm_rle/src/GPR_UNIT_X.vhd
@@ -0,0 +1,43 @@
+-------------------------------------------------------------------------------
+--
+-- Title       : GPR_UNIT_X
+-- Design      : fsm_rle
+-- Author      : maxim
+-- Company     : none
+--
+-------------------------------------------------------------------------------
+--
+-- File        : GPR_UNIT_X.vhd
+-- Generated   : Sat Nov 30 13:57:17 2019
+-- From        : interface description file
+-- By          : Itf2Vhdl ver. 1.22
+--
+-------------------------------------------------------------------------------
+--
+-- Description : 
+--
+-------------------------------------------------------------------------------
+
+--{{ Section below this comment is automatically maintained
+--   and may be overwritten
+--{entity {GPR_UNIT_X} architecture {GPR_UNIT_X}}
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all; 
+use FSM_RLE.all;
+
+entity GPR_UNIT_X is 
+	port(
+		GPR_IN : in GPR_UNIT_OUTPUTS;
+		GPR_OUT : out GPR_UNIT_INPUTS
+		);
+end GPR_UNIT_X;
+
+--}} End of automatically maintained section
+
+architecture GPR_UNIT_X of GPR_UNIT_X is
+begin
+
+	 -- enter your statements here --
+
+end GPR_UNIT_X;
--- fsm_rle/src/RAM_UNIT_X.vhd
+++ fsm_rle/src/RAM_UNIT_X.vhd
@@ -1,14 +1,14 @@
 -------------------------------------------------------------------------------
 --
--- Title       : RAM_UNIT_X
+-- Title       : ROM_UNIT_X
 -- Design      : fsm_rle
--- Author      : maxim
--- Company     : none
+-- Author      : Maxim
+-- Company     : None
 --
 -------------------------------------------------------------------------------
 --
--- File        : RAM_UNIT_X.vhd
--- Generated   : Sat Nov 30 13:00:07 2019
+-- File        : ROM_UNIT_X.vhd
+-- Generated   : Wed Nov 13 23:11:03 2019
 -- From        : interface description file
 -- By          : Itf2Vhdl ver. 1.22
 --
@@ -16,39 +16,41 @@
 --
 -- Description : 
 --
--------------------------------------------------------------------------------
+-------------------------------------------------------------------------------	  
 
-library IEEE;				 			
+library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use FSM_RLE.all;
 
-entity RAM_UNIT_X is
+entity ROM_UNIT_X is
 	port(
-		RAM_IN : in RAM_UNIT_OUTPUTS;
-		RAM_OUT : out RAM_UNIT_INPUTS
+		RAM_IN : in RAM_UNIT_OUTPUTS; -- it is output for fsm
+		RAM_OUT : out RAM_UNIT_INPUTS -- it is input for fsm
 		);
-end RAM_UNIT_X;							
+end ROM_UNIT_X;								  
 
-architecture RAM_UNIT_X of RAM_UNIT_X is 
+architecture ROM_UNIT_X of ROM_UNIT_X is 	 
+
+	component RAM_UNIT is
+		port(
+			CLK : in STD_LOGIC;
+			RST : in STD_LOGIC;	 
+			ADR: in BYTE;
+			DATA_IN: in BYTE;
+			IR : in NIBBLE;			 
+			ADR_EN : in STD_LOGIC;
+			DATA_EN : in STD_LOGIC;
+			W_EN : in STD_LOGIC;
+			R_EN : in STD_LOGIC;
+			RAM_DATA : out BYTE
+			);
+	end component;
 	
-	--component RAM_UNIT is
---		port(
---			CLK : in STD_LOGIC;
---			RST : in STD_LOGIC;
---			TOS : in NIBBLE;
---			TOS_1 : in BYTE;
---			IR : in NIBBLE;
---			ADR_SEL : in STD_LOGIC;
---			ADR_EN : in STD_LOGIC;
---			DATA_EN : in STD_LOGIC;
---			W_EN : in STD_LOGIC;
---			R_EN : in STD_LOGIC;
---			RAM_DATA : out BYTE
---			);
---	end component;
+begin									  
 	
-begin
+	U_RAM_UNIT_X: RAM_UNIT port map (
+		RAM_IN.CLK, RAM_IN.RST, RAM_IN.ADR, RAM_IN.DATA, RAM_IN.IR, RAM_IN.ADR_EN, RAM_IN.DATA_EN, RAM_IN.W_EN, RAM_IN.R_EN,
+		RAM_OUT.DATA
+		);
 	
-	-- enter your statements here --
-	
-end RAM_UNIT_X;
+end ROM_UNIT_X;
